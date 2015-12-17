412f984e6bef0964a11bb6491d453c7814d93864
tran.thanh.tuan269 <tran.thanh.tuan269@gmail.com>
2015-12-17 13:57:05 +0700

    commit theme bootstrap

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,8 +7,9 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
-
+	'name'=>'TUHOC.VN',
+    
+        'theme'=>'bootstrap',
 	// preloading 'log' component
 	'preload'=>array('log'),
 
